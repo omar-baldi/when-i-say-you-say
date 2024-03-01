2ee8e0a82f8910b1b3b86fc276b4cfe625f7182b
Omar Baldi <omar.baldi.business@gmail.com>
2024-03-01 14:03:32 +0100

    Refactoring of game provider by using useReducer hook to have a centralized place to store the state

--- src/providers/gameSessionProvider.tsx
+++ src/providers/gameSessionProvider.tsx
@@ -1,38 +1,93 @@
 /* eslint-disable */
 import { createContext, useEffect, useReducer } from "react";
-
-export type APIReducerState = {
-  isLoading: boolean;
-  error: Error | null;
-  challenge: Challenge | null;
-};
-
-export type Challenge = {
-  clue: string;
-  answer: string;
-};
+import type { Challenge, GameReducerAction, GameReducerState } from "../types";
 
 type GameContextOptions = {};
 
 export const GameContext = createContext<GameContextOptions | null>(null);
 
-const initialApiReducerState = {
-  isLoading: true,
+const initialGameReducerState = {
+  isRetrievingChallenge: true,
   error: null,
+  wordTyped: "",
+  hasUserWonChallenge: false,
+  hasUserLostChallenge: false,
   challenge: null,
-} satisfies APIReducerState;
+  guessesAmount: 0,
+} satisfies GameReducerState;
 
 export default function GameSessionProvider({ children }: { children: React.ReactNode }) {
-  const [apiState, updateAPIState] = useReducer(function (
-    state: APIReducerState,
-    updatedState: Partial<APIReducerState>
+  const [gameState, updateGameState] = useReducer(function (
+    state: GameReducerState,
+    action: GameReducerAction
   ) {
-    return {
-      ...state,
-      ...updatedState,
-    };
+    switch (action.type) {
+      case "UPDATE_LOADING": {
+        const { payload: updatedLoadingState } = action;
+        return {
+          ...state,
+          isRetrievingChallenge: updatedLoadingState,
+        };
+      }
+
+      case "UPDATE_ERROR": {
+        const { payload: updatedError } = action;
+        return {
+          ...state,
+          error: updatedError,
+        };
+      }
+
+      case "UPDATE_CHALLENGE": {
+        const { payload: updatedChallenge } = action;
+        return {
+          ...state,
+          challenge: updatedChallenge,
+        };
+      }
+
+      case "ADD_LETTER": {
+        const { payload: letterToAdd } = action;
+        return {
+          ...state,
+          wordTyped: state.wordTyped.concat(letterToAdd),
+        };
+      }
+
+      case "REMOVE_LETTER": {
+        return {
+          ...state,
+          wordTyped: state.wordTyped.slice(0, -1),
+        };
+      }
+
+      case "INCREASE_AMOUNT_WRONG_GUESSES": {
+        return {
+          ...state,
+          guessesAmount: state.guessesAmount + 1,
+        };
+      }
+
+      case "SET_USER_WON": {
+        return {
+          ...state,
+          hasUserWonChallenge: true,
+        };
+      }
+
+      case "SET_USER_LOST": {
+        return {
+          ...state,
+          hasUserLostChallenge: true,
+        };
+      }
+
+      default: {
+        return state;
+      }
+    }
   },
-  initialApiReducerState);
+  initialGameReducerState);
 
   useEffect(() => {
     const mockDelay = 2000;
@@ -45,14 +100,16 @@ export default function GameSessionProvider({ children }: { children: React.Reac
         resolve(randomChallenge);
       }, mockDelay)
     )
-      .then((challenge) => updateAPIState({ challenge }))
-      .catch((error) => updateAPIState({ error }))
-      .finally(() => updateAPIState({ isLoading: false }));
+      .then((challenge) =>
+        updateGameState({ type: "UPDATE_CHALLENGE", payload: challenge })
+      )
+      .catch((error) => updateGameState({ type: "UPDATE_ERROR", payload: error }))
+      .finally(() => updateGameState({ type: "UPDATE_LOADING", payload: false }));
   }, []);
 
-  const content = apiState.isLoading ? (
+  const content = gameState.isRetrievingChallenge ? (
     <h3>Retrieving challenge for you...</h3>
-  ) : apiState.error ? (
+  ) : gameState.error ? (
     <h3>Unfortunately we could not retrieve a challenge. Try it again later!</h3>
   ) : (
     children
