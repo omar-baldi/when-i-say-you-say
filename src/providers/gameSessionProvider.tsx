02d079f2bba8c474c939be5a3c2c8dfb2b60958e
Omar Baldi <omar.baldi.business@gmail.com>
2024-03-03 18:37:45 +0100

    Refactoring of provider logic to prevent user from playing challenge if last session played was less than a day ago

--- src/providers/gameSessionProvider.tsx
+++ src/providers/gameSessionProvider.tsx
@@ -1,7 +1,14 @@
 /* eslint-disable */
 import { createContext, useEffect, useReducer } from "react";
+import { MILLISECONDS_IN_A_DAY } from "../constants";
+import { useLocalStorage } from "../hook/useLocalStorage";
 import gameReducer, { initialGameReducerState } from "../reducers/gameReducer";
-import type { Challenge, GameReducerAction, GameReducerState } from "../types";
+import type {
+  Challenge,
+  GameReducerAction,
+  GameReducerState,
+  GameSessionStorage,
+} from "../types";
 
 type GameContextOptions = {
   gameState: GameReducerState;
@@ -13,7 +20,55 @@ export const GameContext = createContext<GameContextOptions | null>(null);
 export default function GameSessionProvider({ children }: { children: React.ReactNode }) {
   const [gameState, updateGameState] = useReducer(gameReducer, initialGameReducerState);
 
+  //!NOTE: lastSessionPlayed is only set when either the user has won or when he lost
+  const [gameSessionStorage, updateGameSessionStorage] =
+    useLocalStorage<GameSessionStorage>("gameSession", {
+      lastSessionPlayedDate: null,
+      streak: 0,
+      challengesWon: [],
+    });
+
+  //!NOTE: update dependencies array to include "allowedToPlayChallenge"
+  //!NOTE: otherwise when countdown finishes he will not be able to play game
   useEffect(() => {
+    if (!gameState.allowedToPlayChallenge) return;
+
+    /**
+     * Before retrieving the challenge, I need to make
+     * sure that the user is not playing if the last session
+     * played was less than a day ago.
+     *
+     * If less than a day ago -> we inform the user that he needs to wait
+     * and we show a countdown of the time remaining.
+     *
+     * If no value is found or more than a day ago -> we continue with
+     * retrieving the challenge.
+     */
+
+    if (gameSessionStorage.lastSessionPlayedDate) {
+      const today = new Date();
+      const lastSessionPlayedDate =
+        new Date(gameSessionStorage.lastSessionPlayedDate) ?? new Date();
+      const millisecondsDifference = today.getTime() - lastSessionPlayedDate.getTime();
+
+      /**
+       * TODO: better description
+       * We calculate the difference in milliseconds
+       * as we want to prevent the user from accessing
+       * the challenge even if a millisecond have passed.
+       * Using seconds or minutes or hours is not suitable in this case.
+       */
+      if (millisecondsDifference > 0 && millisecondsDifference < MILLISECONDS_IN_A_DAY) {
+        updateGameState({ type: "ALLOWED_TO_PLAY", payload: false });
+        return;
+      }
+
+      updateGameSessionStorage({
+        ...gameSessionStorage,
+        lastSessionPlayedDate: null,
+      });
+    }
+
     const mockDelay = 2000;
     new Promise<Challenge>((resolve) =>
       setTimeout(async function () {
@@ -29,15 +84,7 @@ export default function GameSessionProvider({ children }: { children: React.Reac
       )
       .catch((error) => updateGameState({ type: "UPDATE_ERROR", payload: error }))
       .finally(() => updateGameState({ type: "UPDATE_LOADING", payload: false }));
-  }, []);
-
-  const content = gameState.isRetrievingChallenge ? (
-    <h3>Retrieving challenge for you...</h3>
-  ) : gameState.error ? (
-    <h3>Unfortunately we could not retrieve a challenge. Try it again later!</h3>
-  ) : (
-    children
-  );
+  }, [gameState.allowedToPlayChallenge]);
 
   /**
    * TODO: feature to implement
@@ -56,7 +103,7 @@ export default function GameSessionProvider({ children }: { children: React.Reac
         updateGameState,
       }}
     >
-      {content}
+      {children}
     </GameContext.Provider>
   );
 }
