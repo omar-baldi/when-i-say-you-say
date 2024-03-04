aea6a38ec070e150b85ec7c715fbdc23ceaf146f
Omar Baldi <omar.baldi.business@gmail.com>
2024-03-04 22:24:57 +0100

    Added test to assert correct state update when 2 seconds have passed

--- tests/hooks/useCountdown.test.ts
+++ tests/hooks/useCountdown.test.ts
@@ -1,7 +1,12 @@
 import { useCountdown } from "@/hook/useCountdown";
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
+import { vi } from "vitest";
 
 describe("useCountdown", () => {
+  beforeAll(() => {
+    vi.useFakeTimers();
+  });
+
   it("should custom hook be initialized with correct default values", () => {
     const { result } = renderHook(() => {
       const d = new Date();
@@ -11,5 +16,18 @@ describe("useCountdown", () => {
     expect(result.current.hours).toBe(0);
     expect(result.current.minutes).toBe(0);
     expect(result.current.seconds).toBe(0);
+  });
+
+  it("should update only seconds value when 2 second have passed", async () => {
+    const { result } = renderHook(() => {
+      const d = new Date();
+      d.setDate(d.getDate() + 1);
+      return useCountdown({ eventDate: d });
+    });
+
+    act(() => vi.advanceTimersByTime(1000 * 2));
+    expect(result.current.hours).toBe(23);
+    expect(result.current.minutes).toBe(59);
+    expect(result.current.seconds).toBe(58);
   });
 });
