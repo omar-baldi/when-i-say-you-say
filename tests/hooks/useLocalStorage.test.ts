6e9ba6960af7cf0121b635d0d67e4dcc43428270
Omar Baldi <omar.baldi.business@gmail.com>
2024-03-05 22:02:16 +0100

    Added test to assert state update and localStorage value update on function call

--- tests/hooks/useLocalStorage.test.ts
+++ tests/hooks/useLocalStorage.test.ts
@@ -1,5 +1,5 @@
 import { useLocalStorage } from "@/hook/useLocalStorage";
-import { renderHook } from "@testing-library/react";
+import { act, renderHook } from "@testing-library/react";
 
 describe("useLocalStorage", () => {
   afterEach(() => {
@@ -21,5 +21,23 @@ describe("useLocalStorage", () => {
     });
     const [storageValue] = result.current;
     expect(storageValue).toBe("storedValue");
+  });
+
+  it("should update both storage value and state when invoking function", () => {
+    window.localStorage.setItem("testKey", JSON.stringify("storedValue"));
+    const { result } = renderHook(() => {
+      return useLocalStorage("testKey", "defaultTestValue");
+    });
+
+    act(() => {
+      const [, updateStorageValue] = result.current;
+      updateStorageValue("updatedStoredValue");
+    });
+
+    const [storageValue] = result.current;
+    expect(storageValue).toBe("updatedStoredValue");
+    expect(window.localStorage.getItem("testKey")).toBe(
+      JSON.stringify("updatedStoredValue")
+    );
   });
 });
