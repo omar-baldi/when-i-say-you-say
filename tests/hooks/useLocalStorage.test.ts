87631a64f095d95574ac3746c34a9e526b3908a5
Omar Baldi <omar.baldi.business@gmail.com>
2024-03-05 22:00:13 +0100

    Added test to assert custom hook returns found storage value if storage key is found

--- tests/hooks/useLocalStorage.test.ts
+++ tests/hooks/useLocalStorage.test.ts
@@ -2,11 +2,24 @@ import { useLocalStorage } from "@/hook/useLocalStorage";
 import { renderHook } from "@testing-library/react";
 
 describe("useLocalStorage", () => {
+  afterEach(() => {
+    window.localStorage.clear();
+  });
+
   it("should custom hook return default value provided if no key is found", () => {
     const { result } = renderHook(() => {
       return useLocalStorage("testKey", "defaultTestValue");
     });
     const [storageValue] = result.current;
     expect(storageValue).toBe("defaultTestValue");
+  });
+
+  it("should custom hook return stored value if key is found", () => {
+    window.localStorage.setItem("testKey", JSON.stringify("storedValue"));
+    const { result } = renderHook(() => {
+      return useLocalStorage("testKey", "defaultTestValue");
+    });
+    const [storageValue] = result.current;
+    expect(storageValue).toBe("storedValue");
   });
 });
